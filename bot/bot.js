efe35d1d732ec63e7bc94c05c5ee76bb64f92e28
kwinkich <vankord21@gmail.com>
2024-04-03 17:32:21 +0300

    added bot manage

---
+++ bot/bot.js
@@ -0,0 +1,26 @@
+import TelegramBot from 'node-telegram-bot-api'
+import 'dotenv/config';
+
+export default function launchBot () {
+  const webAppUrl = process.env.WEB_APP_URL;
+  const token = process.env.TOKEN;
+
+  const bot = new TelegramBot(token, {polling: true});
+
+  bot.on('message', async (msg) => {
+    const chatId = msg.chat.id;
+    const  text = msg.text;
+    
+    if(text === '/start'){
+      await bot.sendMessage(chatId, 'Управляй блогом легко и просто', {
+        reply_markup: {
+          inline_keyboard: [
+            [{text: `Управлять блогом ${chatId}`, web_app: {url: webAppUrl}}]
+          ]
+        }
+      })
+    }
+  });
+}
+
+
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import cors from 'cors';
 import 'dotenv/config';
 import express from 'express';
+import launchBot from './bot/bot.js';
 
 import mongoose from 'mongoose';
 import { router } from './router/router.js';
@@ -12,6 +13,8 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.use('/api', router);
+
+export const Bot = launchBot();
 
 async function startApp() {
 	try {
