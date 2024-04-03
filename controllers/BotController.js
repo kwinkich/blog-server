ae61a572f429506fd84cc595f3ae7c6465bf9b8a
kwinkich <vankord21@gmail.com>
2024-04-03 19:31:38 +0300

    update code

--- controllers/BotController.js
+++ controllers/BotController.js
@@ -1,6 +1,6 @@
 class BotController {
 	async getInitData(req, res) {
-		const { initData } = req.body();
+		const { initData } = req.body;
 		console.log(initData);
 		res.status(200).json(initData);
 	}
