efa2b99b96db622e56bab3ddc25c08f7fbf3b593
kwinkich <vankord21@gmail.com>
2024-04-04 19:01:02 +0300

    update

--- controllers/BotController.js
+++ controllers/BotController.js
@@ -1,38 +1,39 @@
 import CryptoJS from 'crypto-js';
 import 'dotenv/config';
+
+const verifyTelegramWebAppData = async (telegramInitData) => {
+	console.log('telegramInitData', telegramInitData);
+	const initData = new URLSearchParams(telegramInitData);
+	const hash = initData.get('hash');
+
+	const userData = {
+		query_id: initData.get('query_id'),
+		user: JSON.parse(initData.get('user')),
+		auth_date: initData.get('auth_date'),
+	};
+	console.log('userData', userData);
+	let dataToCheck = [];
+
+	initData.sort();
+	initData.forEach(
+		(val, key) => key !== 'hash' && dataToCheck.push(`${key}=${val}`)
+	);
+
+	const secret = CryptoJS.HmacSHA256(process.env.TELEGRAM_API, 'WebAppData');
+	const _hash = CryptoJS.HmacSHA256(dataToCheck.join('\n'), secret).toString(
+		CryptoJS.enc.Hex
+	);
+
+	const isVerify = _hash === hash;
+
+	return { isVerify, userData };
+};
+
 class BotController {
 	async getInitData(req, res) {
 		const { initData } = req.body;
 		console.log(initData);
 		res.status(200).json(initData);
-	}
-
-	async verifyTelegramWebAppData(telegramInitData) {
-		console.log('telegramInitData', telegramInitData);
-		const initData = new URLSearchParams(telegramInitData);
-		const hash = initData.get('hash');
-
-		const userData = {
-			query_id: initData.get('query_id'),
-			user: JSON.parse(initData.get('user')),
-			auth_date: initData.get('auth_date'),
-		};
-		console.log('userData', userData);
-		let dataToCheck = [];
-
-		initData.sort();
-		initData.forEach(
-			(val, key) => key !== 'hash' && dataToCheck.push(`${key}=${val}`)
-		);
-
-		const secret = CryptoJS.HmacSHA256(process.env.TELEGRAM_API, 'WebAppData');
-		const _hash = CryptoJS.HmacSHA256(dataToCheck.join('\n'), secret).toString(
-			CryptoJS.enc.Hex
-		);
-
-		const isVerify = _hash === hash;
-
-		return { isVerify, userData };
 	}
 
 	async verifyTelegramInitData(req, res) {
