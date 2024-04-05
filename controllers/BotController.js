2d9fbe54b8bb2b950fc3a870f92e00d075e37b74
kwinkich <vankord21@gmail.com>
2024-04-05 20:16:30 +0300

    update code

--- controllers/BotController.js
+++ controllers/BotController.js
@@ -1,5 +1,5 @@
 import CryptoJS from 'crypto-js';
-import Token from '../schemes/Token';
+import Token from '../schemes/Token.js';
 
 const verifyTelegramWebAppData = async (telegramInitData) => {
 	const initData = telegramInitData;
