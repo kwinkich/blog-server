ac7f6b1f5cc9740f00da7ef4516670d9268cab7f
kwinkich <vankord21@gmail.com>
2024-04-05 21:34:52 +0300

    update code

--- router/router.js
+++ router/router.js
@@ -9,13 +9,13 @@ router.get('/posts', PostController.getPosts);
 router.get('/posts/:id', PostController.getPostById);
 router.post('/posts/create', PostController.createPost);
 router.put('/posts/update/:id', PostController.editPost);
-router.delete('/posts/delete/:id', PostController.deletePost);
+router.post('/posts/delete/:id', PostController.deletePost);
 
 router.get('/news', NewsController.getNews);
 router.get('/news/:id', NewsController.getNewsById);
 router.post('/news/create', NewsController.createNews);
 router.put('/news/update/:id', NewsController.editNews);
-router.delete('/news/delete/:id', NewsController.deleteNews);
+router.post('/news/delete/:id', NewsController.deleteNews);
 
 router.post('/bot', BotController.getInitData);
 router.post('/bot/verify', BotController.verifyTelegramInitData);
